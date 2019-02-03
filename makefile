615447dabc4e6743b992c6f9dfe951bc50f2c2b0
fritz <frkoenig@gmail.com>
2019-02-03 12:26:15 -0800

    setup_strands
    program to setup acceleromters and
    identify strands before running

--- makefile
+++ makefile
@@ -1,12 +1,16 @@
 CC=gcc
 CFLAGS=-Wall -g
 OBJ=fx.o server.o 
+SETUP_OBJ=setup_strands.o
 LIBS=-lpthread
 
 %.o: %.c
 	$(CC) -c -o $@ $^ $(CFLAGS)
 
 lights: $(OBJ)
+	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
+
+setup_strands: $(SETUP_OBJ)
 	$(CC) -o $@ $^ $(CFLAGS) $(LIBS)
 
 .PHONY: clean
---
+++ setup_strands.c
@@ -0,0 +1,74 @@
+#include <arpa/inet.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static const int kStrandCnt = 20;
+
+int createConnection(int *sock, int host) {
+  struct sockaddr_in server;
+
+  *sock = socket(AF_INET, SOCK_DGRAM, 0);
+  if (*sock == -1) {
+    fprintf(stderr, "Could not create socket");
+    return 0;
+  }
+
+  char addr[32];
+  sprintf(addr, "192.168.1.%d", host);
+  server.sin_addr.s_addr = inet_addr(addr);
+  server.sin_family = AF_INET;
+  server.sin_port = htons(5000);
+
+  if (connect(*sock, (struct sockaddr*)&server, sizeof(server)) < 0) {
+    perror("connect failed. Error");
+    return 0;
+  }
+
+  return 1;
+}
+
+void send_cmd(int sock, char *buffer) {
+  if (send(sock, buffer, strlen(buffer), 0) < 0) {
+    fprintf(stderr, "Send failed");
+  }
+  usleep(10000);
+}
+
+int main(int c, char **v) {
+  char cmd_buffer[256];
+
+  uint32_t sample_rate = 16;
+
+  for (int i = 0; i < kStrandCnt; ++i) {
+    int sock;
+    printf("%d\n", i);
+    createConnection(&sock, i + 200);
+
+    // Identify the strand
+    sprintf(cmd_buffer, "b100");
+    send_cmd(sock, cmd_buffer);
+
+    // Set acceleromter on
+    sprintf(cmd_buffer, "a1");
+    send_cmd(sock, cmd_buffer);
+
+    // Set sample rate
+    sprintf(cmd_buffer, "s%d", sample_rate);
+    send_cmd(sock, cmd_buffer);
+
+    // pause
+    getchar();
+
+    // Clear the strand
+    sprintf(cmd_buffer, "r0");
+    send_cmd(sock, cmd_buffer);
+
+    close(sock);
+  }
+
+  return 0;
+}
+
